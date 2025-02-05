2158e670d05e550d8a95334c70ce2b4fd2dc10ee
Sakshi Singh <sakshi23042005@gmail.com>
2025-02-05 06:51:56 +0000

    factorial using lamda function

---
+++ fact_lamda.py
@@ -0,0 +1,14 @@
+# import math
+
+# # Define range
+# start = int(input())
+# end = int(input())
+
+# # Compute factorial using lambda and map
+# factorial_list = list(map(lambda x: math.factorial(x), range(start, end + 1)))
+
+# # Print result
+# print(f"Factorial of numbers from {start} to {end}: {factorial_list}")
+
+
+        
